2c8718fc8d0c9faf65c0ecad7484db2c9db6bec4
omlight95 <onemorelight234@gmail.com>
2023-05-13 02:47:59 +0800

    memory and disk design

---
+++ fio/file_io.go
@@ -0,0 +1,42 @@
+package fio
+
+import "os"
+
+// FileIO 标准系统文件 IO
+type FileIO struct {
+	// 系统文件描述符
+	fd *os.File
+}
+
+// NewFileIOManager 初始化标准文件 IO
+func NewFileIOManager(fileName string) (*FileIO, error) {
+	fd, err := os.OpenFile(
+		fileName,
+		os.O_CREATE|os.O_RDWR|os.O_APPEND,
+		DataFilePerm,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &FileIO{fd: fd}, nil
+}
+
+// Read 从文件的给定位置读取对应的数据
+func (fio *FileIO) Read(b []byte, offset int64) (int, error) {
+	return fio.fd.ReadAt(b, offset)
+}
+
+// Write 写入字节数组到文件中
+func (fio *FileIO) Write(b []byte) (int, error) {
+	return fio.fd.Write(b)
+}
+
+// Sync 持久化数据
+func (fio *FileIO) Sync() error {
+	return fio.fd.Sync()
+}
+
+// Close 关闭文件
+func (fio *FileIO) Close() error {
+	return fio.fd.Close()
+}
