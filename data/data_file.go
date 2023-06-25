18878a1973c667c8b1ca8be208119bbd8c83a3a5
omlight95 <onemorelight234@gmail.com>
2023-06-26 00:24:15 +0800

    读取LogRecord文件

--- data/data_file.go
+++ data/data_file.go
@@ -1,6 +1,17 @@
 package data
 
-import "CometDB/fio"
+import (
+	"CometDB/fio"
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"path/filepath"
+)
+
+var (
+	ErrInvalidCRC = errors.New("invalid crc value, log record maybe corrupted")
+)
 
 // DataFileNameSuffix 文件后缀
 const DataFileNameSuffix = ".data"
@@ -19,18 +30,95 @@ type DataFile struct {
 
 // OpenDataFile 打开新的数据文件
 func OpenDataFile(dirPath string, field uint32) (*DataFile, error) {
-	return nil, nil
+	fileName := filepath.Join(dirPath, fmt.Sprintf("%09d", field)+DataFileNameSuffix)
+	// 初始化 IOManager 管理器接口
+	ioManager, err := fio.NewIOManager(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return &DataFile{
+		FileId:    field,
+		WriteOff:  0,
+		IOManager: ioManager,
+	}, nil
 }
 
-func (df *DataFile) ReadLogRecord(logOffset int64) (*LogRecord, int64, error) {
-	return nil, 0, nil
+// ReadLogRecord 根据offset从数据文件中读取LogRecord
+func (df *DataFile) ReadLogRecord(offset int64) (*LogRecord, int64, error) {
+	fileSize, err := df.IOManager.Size()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// 如果读取到的最大 header 长度已经超过了文件的长度，则只需要读取到文件的末尾即可
+	var headerBytes int64 = maxLogRecordHeaderSize
+	if offset+maxLogRecordHeaderSize > fileSize {
+		headerBytes = fileSize - offset
+	}
+
+	// 读取 Header 信息
+	headerBuf, err := df.readNBytes(headerBytes, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	header, headerSize := decodeLogRecordHeader(headerBuf)
+	// 以下两个条件表表示读取到了文件末尾，直接返回 EOF 错误
+	if header == nil {
+		return nil, 0, io.EOF
+	}
+	if header.crc == 0 && header.keySize == 0 && header.valueSize == 0 {
+		return nil, 0, io.EOF
+	}
+
+	// 取出对应的 key 和 value 的长度
+	keySize, valueSize := int64(header.keySize), int64(header.valueSize)
+	recordSize := keySize + valueSize
+
+	logRecord := &LogRecord{
+		Type: header.recordType,
+	}
+	// 开始读取用户实际存储的 key/value 数据
+	if keySize > 0 || valueSize > 0 {
+		kvBuf, err := df.readNBytes(keySize+valueSize, offset+headerSize)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		// 获取 key 和 value
+		logRecord.Key = kvBuf[:keySize]
+		logRecord.Value = kvBuf[keySize:]
+	}
+
+	// 校验数据的有效性
+	crc := getLogRecordCRC(logRecord, headerBuf[crc32.Size:headerSize])
+	if crc != header.crc {
+		return nil, 0, ErrInvalidCRC
+	}
+	return logRecord, recordSize, nil
 }
 
 func (df *DataFile) Write(buf []byte) error {
+	n, err := df.IOManager.Write(buf)
+	if err != nil {
+		return err
+	}
+	df.WriteOff += int64(n)
 	return nil
 }
 
 // Sync 数据持久化到磁盘
 func (df *DataFile) Sync() error {
-	return nil
+	return df.IOManager.Sync()
+}
+
+// Close 关闭文件
+func (df *DataFile) Close() error {
+	return df.IOManager.Close()
+}
+
+func (df *DataFile) readNBytes(n int64, offset int64) (b []byte, err error) {
+	b = make([]byte, n)
+	_, err = df.IOManager.Read(b, offset)
+	return
 }
--- data/log_record.go
+++ data/log_record.go
@@ -1,5 +1,7 @@
 package data
 
+import "encoding/binary"
+
 type LogRecordType = byte
 
 const (
@@ -7,12 +9,31 @@ const (
 	LogRecordDeleted
 )
 
+// crc type keySize valueSize
+// 4 + 1 + 5 + 5
+const maxLogRecordHeaderSize = binary.MaxVarintLen32*2 + 5
+
 // LogRecord 写入到数据文件的记录
 // 之所以叫日志，是因为数据文件中的数据是追加写入的，类似日志的格式
 type LogRecord struct {
 	Key   []byte
 	Value []byte
 	Type  LogRecordType
+}
+
+// logRecord的头部信息
+type logRecordHeader struct {
+	// crc 校验值
+	crc uint32
+
+	// 标识 LogRecord 的类型
+	recordType LogRecordType
+
+	// key 的长度
+	keySize uint32
+
+	// value的长度
+	valueSize uint32
 }
 
 type LogRecordPos struct {
@@ -25,4 +46,14 @@ type LogRecordPos struct {
 // EncodeLogRecord 对LogRecord进行编码，返回字节数组及长度
 func EncodeLogRecord(record *LogRecord) ([]byte, int64) {
 	return nil, 0
+}
+
+// 对字节数组中的 Header 信息进行解码
+func decodeLogRecordHeader(buf []byte) (*logRecordHeader, int64) {
+	return nil, 0
+}
+
+// 计算LogRecord的CRC
+func getLogRecordCRC(lr *LogRecord, header []byte) uint32 {
+	return 0
 }
