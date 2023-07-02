4268f95303eb7757d7d0dbb1283747f7d5accfe5
omlight95 <onemorelight234@gmail.com>
2023-07-02 21:16:38 +0800

    支持 redis set 类型

---
+++ redis/set.go
@@ -0,0 +1,120 @@
+package redis
+
+import (
+	"CometDB"
+	"encoding/binary"
+)
+
+type setInternalKey struct {
+	key     []byte
+	version int64
+	member  []byte
+}
+
+func (s *setInternalKey) encode() []byte {
+	buf := make([]byte, len(s.key)+len(s.member)+8+4)
+	// key
+	var index = 0
+	copy(buf[index:index+len(s.key)], s.key)
+	index += len(s.key)
+
+	// version
+	binary.LittleEndian.PutUint64(buf[index:index+8], uint64(s.version))
+	index += 8
+
+	// member
+	copy(buf[index:index+len(s.member)], s.member)
+	index += len(s.member)
+
+	// member size
+	binary.LittleEndian.PutUint32(buf[index:], uint32(len(s.member)))
+
+	return buf
+}
+
+func (r *RedisObject) SAdd(key, member []byte) (bool, error) {
+	// 查找元数据
+	meta, err := r.findMetadata(key, Set)
+	if err != nil {
+		return false, err
+	}
+
+	// 构造一个数据部分的 key
+	s := &setInternalKey{
+		key:     key,
+		version: meta.version,
+		member:  member,
+	}
+
+	var ok bool
+	if _, err = r.db.Get(s.encode()); err == CometDB.ErrKeyNotFound {
+		// 不存在的话则更新
+		wb := r.db.NewWriteBatch(CometDB.DefaultWriteBatchOptions)
+		meta.size++
+		_ = wb.Put(key, meta.encode())
+		_ = wb.Put(s.encode(), nil)
+		if err = wb.Commit(); err != nil {
+			return false, err
+		}
+		ok = true
+	}
+
+	return ok, nil
+}
+
+func (r *RedisObject) SIsMember(key, member []byte) (bool, error) {
+	meta, err := r.findMetadata(key, Set)
+	if err != nil {
+		return false, err
+	}
+	if meta.size == 0 {
+		return false, nil
+	}
+
+	// 构造一个数据部分的 key
+	s := &setInternalKey{
+		key:     key,
+		version: meta.version,
+		member:  member,
+	}
+
+	_, err = r.db.Get(s.encode())
+	if err != nil && err != CometDB.ErrKeyNotFound {
+		return false, err
+	}
+	if err == CometDB.ErrKeyNotFound {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (r *RedisObject) SRem(key, member []byte) (bool, error) {
+	meta, err := r.findMetadata(key, Set)
+	if err != nil {
+		return false, err
+	}
+	if meta.size == 0 {
+		return false, nil
+	}
+
+	// 构造一个数据部分的 key
+	s := &setInternalKey{
+		key:     key,
+		version: meta.version,
+		member:  member,
+	}
+
+	if _, err = r.db.Get(s.encode()); err == CometDB.ErrKeyNotFound {
+		return false, nil
+	}
+
+	// 更新
+	wb := r.db.NewWriteBatch(CometDB.DefaultWriteBatchOptions)
+	meta.size--
+	_ = wb.Put(key, meta.encode())
+	_ = wb.Delete(s.encode())
+	if err = wb.Commit(); err != nil {
+		return false, err
+	}
+	return true, nil
+}
---
+++ redis/set_test.go
@@ -0,0 +1,69 @@
+package redis
+
+import (
+	"CometDB"
+	"CometDB/utils"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestRedisDataStructure_SIsMember(t *testing.T) {
+	opts := CometDB.DefaultOptions
+	dir, _ := os.MkdirTemp("", "CometDB-redis-sismember")
+	opts.DirPath = dir
+	rds, err := NewRedisObject(opts)
+	assert.Nil(t, err)
+
+	ok, err := rds.SAdd(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+	ok, err = rds.SAdd(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.False(t, ok)
+	ok, err = rds.SAdd(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+
+	ok, err = rds.SIsMember(utils.GetTestKey(2), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.False(t, ok)
+	ok, err = rds.SIsMember(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+	ok, err = rds.SIsMember(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+	ok, err = rds.SIsMember(utils.GetTestKey(1), []byte("val-not-exist"))
+	assert.Nil(t, err)
+	assert.False(t, ok)
+}
+
+func TestRedisDataStructure_SRem(t *testing.T) {
+	opts := CometDB.DefaultOptions
+	dir, _ := os.MkdirTemp("", "CometDB-redis-srem")
+	opts.DirPath = dir
+	rds, err := NewRedisObject(opts)
+	assert.Nil(t, err)
+
+	ok, err := rds.SAdd(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+	ok, err = rds.SAdd(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.False(t, ok)
+	ok, err = rds.SAdd(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+
+	ok, err = rds.SRem(utils.GetTestKey(2), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.False(t, ok)
+	ok, err = rds.SRem(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.True(t, ok)
+
+	ok, err = rds.SIsMember(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.False(t, ok)
+}
